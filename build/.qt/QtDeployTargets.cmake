f6773d5feab9f93f1cfdb79cadebeb359898f64c
liujian <905314863@qq.com>
2026-04-24 16:38:50 +0800

    恢复所有修改，清除build文件夹

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VideoPlayer_FILE C:/my-github-project/02.1-videoPlay/videoPlay/bin/VideoPlayer.exe)
-set(__QT_DEPLOY_TARGET_VideoPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VideoPlayer_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
--- build/CMakeFiles/VideoPlayer.dir/VideoPlayer_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/VideoPlayer.dir/VideoPlayer_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\my-github-project\02.1-videoPlay\videoPlay\build\VideoPlayer_autogen\EWIEGA46WW\qrc_resources.cpp
